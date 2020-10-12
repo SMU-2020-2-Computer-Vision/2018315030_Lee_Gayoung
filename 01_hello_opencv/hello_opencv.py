3d053e690eff4e72a62031fce14638949b0f9a2d
LeeGaYoung <lky1732@gmail.com>
2020-10-12 22:48:25 +0900

    drawing

--- 01_hello_opencv/hello_opencv.py
+++ 01_hello_opencv/hello_opencv.py
@@ -7,6 +7,13 @@ os.chdir(cwd)
 
 img = cv2.imread('butterfly.jpg')
 
+print(type(img))
+print(img.ndim)  
+print(img.shape)
+print(img.size)
+print(img.dtype)
+
+
 cv2.imshow('image' ,img)
 
 cv2.waitKey(0)
